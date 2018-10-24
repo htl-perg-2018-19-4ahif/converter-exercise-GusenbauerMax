e08c2f00d5367435f70d4e17a6781a03b357a2e4
Max Gusenbauer <max.gusenb@gmail.com>
2018-10-24 21:16:30 +0200

    finished the program

--- convert.js
+++ convert.js
@@ -1,1 +1,8 @@
-console.log('Not implemented yet!');
+try {
+    var convert = require('convert-units');
+    if (process.argv [4] != 'to' || isNaN(process.argv [2]) ) console.log('Invalid parameters');
+    else console.log(process.argv[2] + ' ' + process.argv[3] + ' are ' + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + ' ' + process.argv[5] );
+}
+catch(err) {
+    console.log('Invalid parameters');
+}
